c7c4407d06c07f4f7175c2f794032420230ac42f
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-06-04 17:03:59 -0700

    Clean up controller.ino

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,56 +1,12 @@
-/*
-  Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  version 2 as published by the Free Software Foundation.
-*/
-
-/**
- * Example for Getting Started with nRF24L01+ radios. 
- *
- * This is an example of how to use the RF24 class.  Write this sketch to two 
- * different nodes.  Put one of the nodes into 'transmit' mode by connecting 
- * with the serial monitor and sending a 'T'.  The ping node sends the current 
- * time to the pong node, which responds by sending the value back.  The ping 
- * node can then see how long the whole cycle took.
- */
-
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 
-//
-// Hardware configuration
-//
-
-// Set up nRF24L01 radio on SPI bus plus pins 9 & 10 
-
 RF24 radio(9,10);
-
-//
-// Topology
-//
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-
-//
-// Role management
-//
-// Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.  
-//
-
-// The various roles supported by this sketch
-/* typedef enum { role_ping_out = 1, role_pong_back } role_e; */
-
-// The debug-friendly names of those roles
-/* const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"}; */
-
-// The role of the current running sketch
-/* role_e role = role_pong_back; */
 
 // button
 const unsigned short nButtons = 2;
@@ -74,19 +30,9 @@ unsigned short getButtons(const unsigned short nButtons,
   return buttons;
 }
 
-void setup(void)
-{
-  //
-  // Print preamble
-  //
-
+void setup(void){
   Serial.begin(57600);
   printf_begin();
-  delay(500);
-
-  //
-  // Setup and configure rf radio
-  //
 
   radio.begin();
 
@@ -97,196 +43,33 @@ void setup(void)
   // improve reliability
   radio.setPayloadSize(sizeof(unsigned short));
 
-  // acknowledge payload
-  radio.enableAckPayload();
-
-  //
-  // Open pipes to other nodes for communication
-  //
-
-  // This simple sketch opens two pipes for these two nodes to communicate
-  // back and forth.
-  // Open 'our' pipe for writing
-  // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-
-  /* if ( role == role_ping_out ) */
-  /*   { */
   radio.openWritingPipe(pipes[0]);
-  /* radio.openReadingPipe(1,pipes[1]); */
-  /* } */
-  /* else */
-  /*   { */
-  /*     radio.openWritingPipe(pipes[1]); */
-  /*     radio.openReadingPipe(1,pipes[0]); */
-  /*   } */
-
-  //
-  // Start listening
-  //
-
-  /* radio.startListening(); */
-
-
-  //
-  // setup keyboard
-  //
-  /* Keyboard.begin(); */
-
-  for(int i = 0; i < 10; ++i){
-    printf("%d\n\r",i);
-    delay(1000);
-  }
 
   unsigned short i;
   for(i = 0; i < nButtons; ++i)
     pinMode(buttonPin[i],INPUT);
   
-  //
-  // Dump the configuration of the rf unit for debugging
-  //
-
   curr = prev = 0;
-
-  radio.printDetails();
 }
 
 
 
-void loop(void)
-{
-  //
-  // Ping out role.  Repeatedly send the current time
-  //
-
-  /* if (role == role_ping_out) */
-  /* int buttonState = digitalRead(buttonPin[0]); */
-  
+void loop(void){
   curr = getButtons(nButtons,buttonPin);
   
-  if(curr != prev)
-    {
-      printf("Buttons: %u\n\r",curr);
+  if(curr != prev){
+    printf("Buttons: %u\n\r",curr);
   
-      // First, stop listening so we can talk.
-      /* radio.stopListening(); */
-
-      // Take the time, and send it.  This will block until complete
-      /* unsigned long time = millis(); */
-      /* printf("Now sending %lu...",time); */
-      bool ok = radio.write( &curr, sizeof(unsigned short) );
-      prev = curr;
+    bool ok = radio.write( &curr, sizeof(unsigned short) );
+    prev = curr;
     
-      if (ok)
-	/* printf("ok..."); */
-	printf("sent.\n\r");
-      else
-	printf("failed.\n\r");
+    if (ok)
+      printf("sent.\n\r");
+    else
+      printf("failed.\n\r");
 
-      /* delay(1000); */
-
-      /* if(radio.isAckPayloadAvailable()){ */
-      /* 	unsigned short check; */
-      /* 	radio.read(&check,sizeof(unsigned short)); */
-      /* 	printf("Ack payload: %u.\n\r",check); */
-      /* } */
-      
-      // Now, continue listening
-      radio.startListening();
-      radio.stopListening();
-      /* if(radio.available()){} */
-
-      // Wait here until we get a response, or timeout (250ms)
-      /* unsigned long started_waiting_at = millis(); */
-      /* bool timeout = false; */
-      /* while ( ! radio.available() && ! timeout ) */
-      /* 	if (millis() - started_waiting_at > 200 ) */
-      /* 	  timeout = true; */
-
-      /* // Describe the results */
-      /* if ( timeout ) */
-      /* 	{ */
-      /* 	  printf("Failed, response timed out.\n\r"); */
-      /* 	} */
-      /* else */
-      /* 	{ */
-      /* 	  // Grab the response, compare, and send to debugging spew */
-      /* 	  unsigned short got_buttons; */
-      /* 	  radio.read( &got_buttons, sizeof(unsigned short) ); */
-
-      /* 	  // Spew it */
-      /* 	  printf("Got response %u\n\r",got_buttons); */
-      /* 	} */
-
-    }
-
-  // Try again 1s later
-  /* delay(1000); */
-  
-  //
-  // Pong back role.  Receive each packet, dump it out, and send it back
-  //
-
-  /* if ( role == role_pong_back ) */
-  /*   { */
-  /*     // if there is data ready */
-  /*     if ( radio.available() ) */
-  /* 	{ */
-  /* 	  // Dump the payloads until we've gotten everything */
-  /* 	  unsigned long got_time; */
-  /* 	  bool done = false; */
-  /* 	  while (!done) */
-  /* 	    { */
-  /* 	      // Fetch the payload, and see if this was the last one. */
-  /* 	      done = radio.read( &got_time, sizeof(unsigned long) ); */
-
-  /* 	      // Spew it */
-  /* 	      printf("Got payload %lu...",got_time); */
-
-  /* 	      // Delay just a little bit to let the other unit */
-  /* 	      // make the transition to receiver */
-  /* 	      delay(20); */
-  /* 	    } */
-
-  /* 	  printf("write..."); */
-  /* 	  Keyboard.write(KEY_RETURN); */
-
-  /* 	  // First, stop listening so we can talk */
-  /* 	  radio.stopListening(); */
-
-  /* 	  // Send the final one back. */
-  /* 	  radio.write( &got_time, sizeof(unsigned long) ); */
-  /* 	  printf("Sent response.\n\r"); */
-
-  /* 	  // Now, resume listening so we catch the next packets. */
-  /* 	  radio.startListening(); */
-  /* 	} */
-  /*   } */
-
-  //
-  // Change roles
-  //
-
-  /* if ( Serial.available() ) */
-  /* { */
-  /*   int buttonState = digitalRead(buttonPin); */
-  /*   if ( buttonState == HIGH && role == role_pong_back ) */
-  /* 	{ */
-  /* 	  /\* printf("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK\n\r"); *\/ */
-
-  /* 	  // Become the primary transmitter (ping out) */
-  /* 	  role = role_ping_out; */
-  /* 	  radio.openWritingPipe(pipes[0]); */
-  /* 	  radio.openReadingPipe(1,pipes[1]); */
-  /* 	} */
-  /*   else if ( buttonState == LOW && role == role_ping_out ) */
-  /* 	{ */
-  /* 	  /\* printf("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK\n\r"); *\/ */
-      
-  /* 	  // Become the primary receiver (pong back) */
-  /* 	  role = role_pong_back; */
-  /* 	  radio.openWritingPipe(pipes[1]); */
-  /* 	  radio.openReadingPipe(1,pipes[0]); */
-  /* 	} */
-  /* } */
+    // have to cycle listening otherwise communication fails
+    radio.startListening();
+    radio.stopListening();
+  }
 }
-// vim:cin:ai:sts=2 sw=2 ft=cpp
