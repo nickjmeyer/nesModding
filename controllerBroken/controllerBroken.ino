23c8bba4b3dd350be88077d3eba7a8e9cbb80b8a
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-07-03 12:55:50 -0700

    Testing new setup with ack payloads

---
+++ controllerBroken/controllerBroken.ino
@@ -0,0 +1,79 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+RF24 radio(9,10);
+
+// Radio pipe addresses for the 2 nodes to communicate.
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+// button
+const unsigned short nButtons = 2;
+const unsigned short buttonPin[] = {6,7};
+
+unsigned curr,prev;
+
+
+unsigned short getButtons(const unsigned short nButtons,
+			  const unsigned short * const buttonPin);
+
+unsigned short getButtons(const unsigned short nButtons,
+			  const unsigned short * const buttonPin){
+  // write each button to a bit
+  unsigned short i,buttons = 0,state;
+  for(i = 0; i < nButtons; ++i){
+    state = digitalRead(buttonPin[i]);
+    if(state == HIGH){
+      buttons |= (1u << i);
+    }
+  }
+  return buttons;
+}
+
+void setup(void){
+  Serial.begin(57600);
+  printf_begin();
+
+  radio.begin();
+
+  // optionally, increase the delay between retries & # of retries
+  radio.setRetries(15,15);
+
+  // turn off auto ack
+  radio.setAutoAck(0);
+
+  // optionally, reduce the payload size.  seems to
+  // improve reliability
+  radio.setPayloadSize(sizeof(unsigned short));
+
+  radio.openWritingPipe(pipes[0]);
+
+  unsigned short i;
+  for(i = 0; i < nButtons; ++i)
+    pinMode(buttonPin[i],INPUT);
+  
+  curr = prev = 0;
+}
+
+
+
+void loop(void){
+  curr = getButtons(nButtons,buttonPin);
+  
+  if(curr != prev){
+    printf("Buttons: %u\n\r",curr);
+  
+    bool ok = radio.write( &curr, sizeof(unsigned short) );
+    prev = curr;
+    
+    if (ok)
+      printf("sent.\n\r");
+    else
+      printf("failed.\n\r");
+
+    // have to cycle listening otherwise communication fails
+    /* radio.startListening(); */
+    /* radio.stopListening(); */
+  }
+}
--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,78 +1,179 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
 
+//
+// Hardware configuration
+//
+
+// Set up nRF24L01 radio on SPI bus plus pins 9 & 10
+
 RF24 radio(9,10);
 
-// Radio pipe addresses for the 2 nodes to communicate.
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+// sets the role of this unit in hardware.  Connect to GND to be the 'pong' receiver
+// Leave open to be the 'ping' transmitter
+const short role_pin = 7;
 
-const unsigned short nButtons = 2;
-const unsigned short keys[] = {216, // left
-			       215}; // right
-const char* name[] = {"LEFT","RIGHT"};
+//
+// Topology
+//
 
-unsigned short curr = 0,prev = 0;
+// Single radio pipe address for the 2 nodes to communicate.
+const uint64_t pipe = 0xE8E8F0F0E1LL;
 
-void keyMap(const unsigned short curr, const unsigned short prev,
-	    const unsigned short nButtons){
-  // map the button changes to keyboard commands
-  
-  unsigned short i,change;
-  change = curr ^ prev;
-  for(i = 0; i < nButtons; ++i){
-    if((change & (1u << i)) && (curr & (1u << i))){
-      printf("Press %s\n\r",name[i]);
-      Keyboard.press(keys[i]);
-    }
-    else if(change & (1u << i)){
-      printf("Release %s\n\r",name[i]);
-      Keyboard.release(keys[i]);
-    }
-  }
-}
+//
+// Role management
+//
+// Set up role.  This sketch uses the same software for all the nodes in this
+// system.  Doing so greatly simplifies testing.  The hardware itself specifies
+// which node it is.
+//
+// This is done through the role_pin
+//
 
-void setup(void){
+// The various roles supported by this sketch
+typedef enum { role_sender = 1, role_receiver } role_e;
+
+// The debug-friendly names of those roles
+const char* role_friendly_name[] = { "invalid", "Sender", "Receiver"};
+
+// The role of the current running sketch
+role_e role;
+
+void setup(void)
+{
+  //
+  // Role
+  //
+
+  // set up the role pin
+  pinMode(role_pin, INPUT);
+  digitalWrite(role_pin,HIGH);
+  delay(20); // Just to get a solid reading on the role pin
+
+  // read the address pin, establish our role
+  if ( digitalRead(role_pin) )
+    role = role_sender;
+  else
+    role = role_receiver;
+
+  //
+  // Print preamble
+  //
+
   Serial.begin(57600);
   printf_begin();
 
+  int i;
+  for(i = 0; i < 10; ++i){
+    printf("% 3d\n\r",i);
+    delay(1000);
+  }
+
+
+  printf("\n\rRF24/examples/pingpair_pl/\n\r");
+  printf("ROLE: %s\n\r",role_friendly_name[role]);
+
+  //
+  // Setup and configure rf radio
+  //
+
   radio.begin();
 
-  // optionally, increase the delay between retries & # of retries
-  radio.setRetries(15,15);
+  // We will be using the Ack Payload feature, so please enable it
+  radio.enableAckPayload();
 
-  // turn off auto ack
-  radio.setAutoAck(0);
+  //
+  // Open pipes to other nodes for communication
+  //
 
-  // optionally, reduce the payload size.  seems to
-  // improve reliability
-  radio.setPayloadSize(sizeof(unsigned short));
+  // This simple sketch opens a single pipes for these two nodes to communicate
+  // back and forth.  One listens on it, the other talks to it.
 
-  radio.openReadingPipe(1,pipes[0]);
+  if ( role == role_sender )
+  {
+    radio.openWritingPipe(pipe);
+  }
+  else
+  {
+    radio.openReadingPipe(1,pipe);
+  }
 
-  radio.startListening();
+  //
+  // Start listening
+  //
 
-  Keyboard.begin();
+  if ( role == role_receiver )
+    radio.startListening();
 
-  curr = prev = 0;
+  //
+  // Dump the configuration of the rf unit for debugging
+  //
+
+  radio.printDetails();
 }
 
-void loop(void){
-  // if there is data ready
-  if ( radio.available() ){
-    // Dump the payloads until we've gotten everything
-    unsigned short got_buttons;
-    bool done = false;
-    while (!done){
-      // Fetch the payload, and see if this was the last one.
-      done = radio.read( &got_buttons, sizeof(unsigned short) );
-      curr = got_buttons;
+void loop(void)
+{
+  static uint32_t message_count = 0;
 
-      keyMap(curr,prev,nButtons);
-      prev = curr;
+  //
+  // Sender role.  Repeatedly send the current time
+  //
 
-      printf("Got payload %u.\n\r",got_buttons);
+  if (role == role_sender)
+  {
+    // Take the time, and send it.  This will block until complete
+    unsigned long time = millis();
+    printf("Now sending %lu...",time);
+    radio.write( &time, sizeof(unsigned long) );
+
+    if ( radio.isAckPayloadAvailable() )
+    {
+      radio.read(&message_count,sizeof(message_count));
+      printf("Ack: [%lu] ",message_count);
+    }
+    printf("OK\n\r");
+
+    // Try again soon
+    delay(2000);
+  }
+
+  //
+  // Receiver role.  Receive each packet, dump it out, add ack payload for next time
+  //
+
+  if ( role == role_receiver )
+  {
+    // if there is data ready
+    if ( radio.available() )
+    {
+      // Dump the payloads until we've gotten everything
+      static unsigned long got_time;
+      bool done = false;
+      while (!done)
+      {
+        // Fetch the payload, and see if this was the last one.
+        done = radio.read( &got_time, sizeof(unsigned long) );
+
+        // Spew it
+        printf("Got payload %lu\n",got_time);
+      }
+
+      // Add an ack packet for the next time around.  This is a simple
+      // packet counter
+      radio.writeAckPayload( 1, &message_count, sizeof(message_count) );
+      ++message_count;
     }
   }
 }
+// vim:ai:cin:sts=2 sw=2 ft=cpp
