eba3efcd4a93774842190bf4f44f980c75a8971a
Nicholas J Meyer <nick.j.meyer@gmail.com>
2015-08-22 17:23:44 -0400

    Place the register read into a function

--- nesInput/nesInput.ino
+++ nesInput/nesInput.ino
@@ -2,39 +2,66 @@
 #include "printf.h"
 
 
-typedef unsigned int uint;
+const unsigned short nButtons = 12;
 
-const uint nButtons = 12;
+const unsigned short clock = 7;
+const unsigned short latch = 6;
+const unsigned short data = 5;
+
+
+unsigned short prev, curr;
+
+
+unsigned short readController(const unsignes short nButtons,
+			      const unsigned short clock,
+			      const unsigned short latch,
+			      const unsigned short data);
+
+unsigned short readController(const unsigned short nButtons,
+			      const unsigned short clock,
+			      const unsigned short latch,
+			      const unsigned short data){
+  digitalWrite(latch,HIGH);
+  /* delay(100); */
+  digitalWrite(latch,LOW);
+  unsigned short i;
+  unsigned short buttons = 0;
+  unsigned short swap = 1;
+  for(i = 0; i < nButtons; ++i){
+    digitalWrite(clock,LOW);
+    /* delay(10); */
+    if(digitalRead(data) == LOW){
+      buttons |= swap;
+    }
+    swap <<= 1;
+
+    digitalWrite(clock,HIGH);
+  }
+  return buttons;
+}
+
 
 
 void setup(void){
   Serial.begin(57600);
   printf_begin();
 
-  pinMode(7,OUTPUT); // clock (blue)
-  pinMode(6,OUTPUT); // latch (yellow)
-  pinMode(5,INPUT); // data (red)
+  pinMode(clock,OUTPUT); // clock (blue)
+  pinMode(latch,OUTPUT); // latch (yellow)
+  pinMode(data,INPUT); // data (red)
 
   // ground (black)
   // 5v (white)
+
+  prev = curr = 0;
 }
 
 
 
 void loop(void){
-  digitalWrite(6,HIGH);
-  /* delay(100); */
-  digitalWrite(6,LOW);
-  uint i;
-  printf("[");
-  for(i = 0; i < nButtons; ++i){
-    digitalWrite(7,LOW);
-    /* delay(10); */
-    printf("%d",digitalRead(5));
-
-    digitalWrite(7,HIGH);
-    if((i+1) < nButtons)
-      printf(", ");
+  unsigned short curr = readController(nButtons,clock,latch,data);
+  if(curr != prev){
+    printf("%hu\n\r",curr);
+    prev = curr;
   }
-  printf("]\n\r");
 }
